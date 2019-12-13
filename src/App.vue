603e17316eb4db7119746d3ee8f2160e86ebf50b
Cal027 <11710809@mail.sustech.edu.cn>
2019-12-14 00:53:31 +0800

    add: logo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo251.png" class="logo">
+    <el-upload drag action="#" :auto-upload="false" ref="upload">
+      <i class="el-icon-upload"/>
+      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+    </el-upload>
+    <br/>
+    <el-button icon="el-icon-upload2" size="small" round @click="handleSubmit">上传251</el-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    handleSubmit () {
+      this.$message({
+        type: 'success',
+        message: '上传到华为ModelArt',
+        duration: 1500,
+        center: true
+      })
+      this.$refs.upload.submit()
+    }
   }
 }
 </script>
@@ -23,6 +40,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 40px;
+
+  .logo{
+    width: 236px;
+    height: 321px;
+  }
 }
 </style>

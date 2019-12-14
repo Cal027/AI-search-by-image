eb973bd6536df2d8a017e383e7c1fc65e77138a5
Cal027 <11710809@mail.sustech.edu.cn>
2019-12-14 15:30:12 +0800

    fix:bug

--- src/components/Upload251.vue
+++ src/components/Upload251.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <el-upload drag :action="api"
+    <el-upload drag action=""
                :auto-upload="false"
                ref="upload"
                accept=".jpg, .jpeg, .png"
+               :on-error="onSuccess"
                :before-upload="beforeUpload">
       <i class="el-icon-upload"/>
       <div class="el-upload__text">将图片拖到此处，或<em>点击上传</em></div>
@@ -27,7 +28,7 @@
                 <span>{{obj.class}}</span>
                 <el-button style="margin-left: 5px"
                            icon="el-icon-search"
-                           @click="searchImg"
+                           @click="searchImg(index)"
                            type="success" circle
                            size="mini"/>
               </div>
@@ -45,23 +46,24 @@ export default {
   name: 'Upload251',
   data () {
     return {
-      api: '#',
+      api: 'https://4c7298efd5d44a09b1affdafcf7ee5d6.apigw.cn-north-4.huaweicloud.com/v1/infers/0d69f607-0105-4767-8157-38ac4a927bd0',
+      token: '',
       showRes: false,
       img: null,
       detectObjects: [
         {
           'box': [
             90,
-            970,
+            270,
             382,
-            1061
+            106
           ],
           'class': 'person'
         },
         {
           'box': [
             15,
-            726,
+            226,
             50,
             242
           ],
@@ -69,8 +71,8 @@ export default {
         },
         {
           'box': [
-            15,
-            726,
+            45,
+            126,
             50,
             242
           ],
@@ -93,6 +95,15 @@ export default {
       this.viewImage(file)
       this.showRes = true
     },
+    onSuccess (response, file, fileList) {
+      this.$refs.item[0].setCoordinates({
+        width: 200,
+        height: 200,
+        left: 0,
+        top: 0
+      }
+      )
+    },
     viewImage (file) {
       const reader = new FileReader()
       reader.onload = (e) => {
@@ -100,7 +111,7 @@ export default {
       }
       reader.readAsDataURL(file)
     },
-    searchImg () {
+    searchImg (index) {
 
     },
     cleanImg () {
@@ -111,6 +122,15 @@ export default {
         center: true
       })
       this.img = null
+      this.showRes = false
+    }
+  },
+  mounted () {
+    // 获取token
+    if (localStorage.token) {
+      this.token = localStorage.token
+    } else {
+      // this.token = ''
     }
   }
 }

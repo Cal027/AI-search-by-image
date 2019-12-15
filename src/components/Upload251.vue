e0699219955dcaec9be969d40abc90ea0a6b1a3a
Jonhao Leung <11710911@mail.sustech.edu,cn>
2019-12-15 16:55:50 +0800

    add: 实现google搜图

--- src/components/Upload251.vue
+++ src/components/Upload251.vue
@@ -152,8 +152,31 @@ export default {
     },
     // 以图搜图
     searchImg (index) {
-
+      const data = new FormData()
+      data.append('encoded_image', this.dataURLtoBlob(this.getImage(index)), 'image.png')
+      fetch('https://www.google.com/searchbyimage/upload', {
+        referrer: '',
+        mode: 'cors',
+        method: 'POST',
+        body: data
+      }).then(data => {
+        window.open(data.url)
+      })
+      // console.log(rsp)
+      // window.location.replace(rsp.url)
     },
+    dataURLtoBlob (dataurl) {
+      var arr = dataurl.split(',')
+      var mime = arr[0].match(/:(.*?);/)[1]
+      var bstr = atob(arr[1])
+      var n = bstr.length
+      var u8arr = new Uint8Array(n)
+      while (n--) {
+        u8arr[n] = bstr.charCodeAt(n)
+      }
+      return new Blob([u8arr], { type: mime })
+    },
+
     // 裁剪并保存图片
     cropImage () {
       this.$message({
